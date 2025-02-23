54b99cf9bd579ece9f5bbf4d3d8daa532bde0e50
Gowthaman Mathaiyan <gowthaman999@live.in>
2025-02-23 17:15:52 +0530

    Removing group related code

--- src/main/java/com/poc/microservice/config/SpringDocConfig.java
+++ src/main/java/com/poc/microservice/config/SpringDocConfig.java
@@ -4,18 +4,9 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springdoc.core.models.GroupedOpenApi;
 
 @Configuration
 public class SpringDocConfig {
-    @Bean
-    public GroupedOpenApi publicApi() {
-        return GroupedOpenApi.builder()
-                .group("public")
-                .pathsToMatch("/api/**")
-                .build();
-    }
-
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
