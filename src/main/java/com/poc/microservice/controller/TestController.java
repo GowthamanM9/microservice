e7c10a84fe71d0f2d55fd2b1a8c75d6998f8b851
Gowthaman Mathaiyan <gowthaman999@live.in>
2025-02-25 20:38:04 +0530

    Adding(increasing) time out for build

--- src/main/java/com/poc/microservice/controller/TestController.java
+++ src/main/java/com/poc/microservice/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.poc.microservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,7 @@ public class TestController {
     }
 
     @GetMapping("/static-string")
-    public String getStaticString() {
-        return "This is a static string after bc, dc and config yml changes.";
+    public String getStaticString(@RequestHeader(value = "X-Auth-Token", required = false) String token) {
+        return "This is a static string after bc, dc and config yml changes. Token received: " + (token != null ? token : "none");
     }
 }
